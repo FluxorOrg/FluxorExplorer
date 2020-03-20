57d5adac7238725f19cc9bd2e5c288738d1f8d6b
Morten Bjerg Gregersen <morten@mogee.dk>
2020-03-20 23:05:42 +0100

    Remove launch performance test

--- FluxorExplorerUITests/FluxorExplorerUITests.swift
+++ FluxorExplorerUITests/FluxorExplorerUITests.swift
@@ -30,13 +30,4 @@ class FluxorExplorerUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
