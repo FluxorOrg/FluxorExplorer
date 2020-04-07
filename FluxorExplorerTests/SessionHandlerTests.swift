18a3905111e29806c1f74aca36c2f72afa36a384
Morten Bjerg Gregersen <morten@mogee.dk>
2020-04-07 22:28:22 +0200

    Fix duplication

--- FluxorExplorerTests/SessionHandlerTests.swift
+++ FluxorExplorerTests/SessionHandlerTests.swift
@@ -15,49 +15,41 @@ import XCTest
 class SessionHandlerTests: XCTestCase {
     let peer = MCPeerID(displayName: "Some peer")
     let serviceType = "fluxor-explorer"
-
-    func testSessionConnected() {
-        // Given
-        let handler = SessionHandler()
-        let session = MCSession(peer: peer)
-        let store = Store(initialState: AppState())
-        let interceptor = TestInterceptor<AppState>()
+    var session: MCSession!
+    var handler: SessionHandler!
+    var interceptor: TestInterceptor<AppState>!
+    var store: Store<AppState>!
+    
+    override func setUp() {
+        session = .init(peer: peer)
+        handler = .init()
+        store = .init(initialState: .init())
+        interceptor = .init()
         store.register(interceptor: interceptor)
         Current.store = store
+    }
+
+    func testSessionConnected() {
         // When
         handler.session(session, peer: peer, didChange: .connected)
         // Then
         waitFor {
-            let action = interceptor.dispatchedActionsAndStates[0].action as! PeerConnectedAction
+            let action = self.interceptor.dispatchedActionsAndStates[0].action as! PeerConnectedAction
             XCTAssertEqual(action.peer, self.peer)
         }
     }
 
     func testSessionNotConnected() {
-        // Given
-        let handler = SessionHandler()
-        let session = MCSession(peer: peer)
-        let store = Store(initialState: AppState())
-        let interceptor = TestInterceptor<AppState>()
-        store.register(interceptor: interceptor)
-        Current.store = store
         // When
         handler.session(session, peer: peer, didChange: .notConnected)
         // Then
         waitFor {
-            let action = interceptor.dispatchedActionsAndStates[0].action as! PeerDisconnectedAction
+            let action = self.interceptor.dispatchedActionsAndStates[0].action as! PeerDisconnectedAction
             XCTAssertEqual(action.peer, self.peer)
         }
     }
 
     func testSessionDidReceiveData() throws {
-        // Given
-        let handler = SessionHandler()
-        let session = MCSession(peer: peer)
-        let store = Store(initialState: AppState())
-        let interceptor = TestInterceptor<AppState>()
-        store.register(interceptor: interceptor)
-        Current.store = store
         // When
         let snapshot = FluxorExplorerSnapshot(action: TestAction(increment: 1),
                                               oldState: TestState(counter: 42),
@@ -66,7 +58,7 @@ class SessionHandlerTests: XCTestCase {
         handler.session(session, didReceive: data, fromPeer: peer)
         // Then
         waitFor {
-            let action = interceptor.dispatchedActionsAndStates[0].action as! DidReceiveSnapshotAction
+            let action = self.interceptor.dispatchedActionsAndStates[0].action as! DidReceiveSnapshotAction
             XCTAssertEqual(action.peer, self.peer)
             XCTAssertEqual(action.snapshot, snapshot)
         }
@@ -74,7 +66,6 @@ class SessionHandlerTests: XCTestCase {
 
     func testBrowserFoundPeer() {
         // Given
-        let handler = SessionHandler()
         let browser = MockBrowser(peer: peer, serviceType: serviceType)
         let otherPeer = MCPeerID(displayName: "Another peer")
         // When
