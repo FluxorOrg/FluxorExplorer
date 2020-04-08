e71ab05ea6d26ec2f72de6f91997d01333e01a5f
Morten Bjerg Gregersen <morten@mogee.dk>
2020-04-08 14:24:11 +0200

    Simplify

--- FluxorExplorer/AppDelegate.swift
+++ FluxorExplorer/AppDelegate.swift
@@ -8,18 +8,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let peerConfiguration = SceneConfiguration.peer
-        if options.userActivities.contains(where: { $0.activityType == peerConfiguration.activityIdentifier }) {
-            return peerConfiguration.sceneConfiguration
-        } else {
-            return SceneConfiguration.default.sceneConfiguration
-        }
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return SceneConfiguration.peer.sceneConfiguration
     }
 }
--- FluxorExplorer/Scenes/PeerSceneDelegate.swift
+++ FluxorExplorer/Scenes/PeerSceneDelegate.swift
@@ -14,14 +14,14 @@ class PeerSceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
         guard let activity = connectionOptions.userActivities.first(where: {
             $0.activityType == SceneConfiguration.peer.activityIdentifier
         }) else {
             UIApplication.shared.requestSceneSessionDestruction(session, options: nil, errorHandler: nil)
             return
         }
-        guard let peerName = activity.userInfo?["peerName"] as? String,
+        guard let windowScene = scene as? UIWindowScene,
+            let peerName = activity.userInfo?["peerName"] as? String,
             let windowStore = Current.storeByPeers[peerName] else { return }
         windowScene.title = peerName
         let peerView = PeerView(store: windowStore)
