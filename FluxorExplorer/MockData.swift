626e8c3c1612a8a141bb7c3d22ed7caf3a863695
Morten Bjerg Gregersen <morten@mogee.dk>
2020-05-29 11:22:12 +0200

    Add mock data for screenshots (#22)
    * Bump FluxorExplorerSnapshot
    
    * Update MockData.swift
    
    * Update MockData.swift
    
    * Break down function
    
    * Fix test

--- FluxorExplorer/MockData.swift
+++ FluxorExplorer/MockData.swift
@@ -9,9 +9,12 @@ import Fluxor
 import FluxorExplorerSnapshot
 import MultipeerConnectivity
 
+// swiftlint:disable trailing_comma
+
 func setupMockData() {
-    let peer1 = MCPeerID(displayName: "Peer 1")
-    let peer2 = MCPeerID(displayName: "Peer 2")
+    let peer1 = MCPeerID(displayName: "iPhone 11 Pro")
+    let peer2 = MCPeerID(displayName: "iPhone 8")
+    let snapshots = createSnapshots()
     Current.store.register(reducer: createReducer(
         reduceOn(ResetStateAction.self) { state, _ in
             state = AppState()
@@ -20,20 +23,87 @@ func setupMockData() {
     Current.store.dispatch(action: ResetStateAction())
     Current.store.dispatch(action: PeerConnectedAction(peer: peer1))
     Current.store.dispatch(action: PeerConnectedAction(peer: peer2))
-    Current.store.dispatch(action: DidReceiveSnapshotAction(
-        peer: peer1,
-        snapshot: FluxorExplorerSnapshot(action: TestAction(increment: 1),
-                                         oldState: TestState(counter: 1),
-                                         newState: TestState(counter: 2))))
+    snapshots.forEach {
+        Current.store.dispatch(action: DidReceiveSnapshotAction(peer: peer1, snapshot: $0))
+    }
 }
 
-private struct TestState: Encodable {
-    let counter: Int
+private var oldDate = Date()
+private var oldState = SampleAppState(todos: TodosState(loadingTodos: false))
+
+private func createSnapshots() -> [FluxorExplorerSnapshot] {
+    var todos = [
+        Todo(title: "Dispatch actions"),
+        Todo(title: "Create effects"),
+        Todo(title: "Select something"),
+        Todo(title: "Intercept everything"),
+    ]
+    let newTodoTitle = "Buy milk"
+    return [
+        createSnapshot(action: FetchTodosAction(), newState: SampleAppState(todos: TodosState(loadingTodos: true))),
+        createSnapshot(action: DidFetchTodosAction(todos: todos),
+                       newState: SampleAppState(todos: TodosState(todos: todos, loadingTodos: false))),
+        createSnapshot(action: CompleteTodoAction(todo: todos[0]),
+                       newState: SampleAppState(todos: TodosState(todos: { todos[0].done = true; return todos }(),
+                                                                  loadingTodos: false))),
+        createSnapshot(action: AddTodoAction(title: newTodoTitle),
+                       newState: SampleAppState(todos: TodosState(todos: {
+                           todos.append(Todo(title: newTodoTitle)); return todos
+        }(), loadingTodos: false))),
+        createSnapshot(action: CompleteTodoAction(todo: todos[1]),
+                       newState: SampleAppState(todos: TodosState(todos: { todos[1].done = true; return todos }(),
+                                                                  loadingTodos: false))),
+        createSnapshot(action: UncompleteTodoAction(todo: todos[1]),
+                       newState: SampleAppState(todos: TodosState(todos: { todos[1].done = false; return todos }(),
+                                                                  loadingTodos: false))),
+    ]
 }
 
-private struct TestAction: Action {
-    let increment: Int
+private func createSnapshot(action: Action, newState: SampleAppState) -> FluxorExplorerSnapshot {
+    let date = oldDate.addingTimeInterval(TimeInterval.random(in: 1...5))
+    defer {
+        oldState = newState
+        oldDate = date
+    }
+    return FluxorExplorerSnapshot(action: action, oldState: oldState, newState: newState, date: date)
+}
+
+private struct SampleAppState: Encodable {
+    var todos = TodosState()
+}
+
+private struct TodosState: Encodable {
+    var todos = [Todo]()
+    var loadingTodos = false
+}
+
+private struct FetchTodosAction: Action {}
+
+private struct DidFetchTodosAction: Action {
+    let todos: [Todo]
+}
+
+private struct AddTodoAction: Action {
+    let title: String
+}
+
+private struct CompleteTodoAction: Action {
+    let todo: Todo
+}
+
+private struct UncompleteTodoAction: Action {
+    let todo: Todo
+}
+
+private struct DeleteTodoAction: Action {
+    let index: Int
 }
 
 private struct ResetStateAction: Action {}
+
+private struct Todo: Codable {
+    let title: String
+    var done = false
+}
+
 #endif
