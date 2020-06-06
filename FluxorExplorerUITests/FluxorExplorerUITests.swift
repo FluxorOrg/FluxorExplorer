3d96ada3978cf832261f0b7380070bcacc9020aa
Morten Bjerg Gregersen <morten@mogee.dk>
2020-06-06 08:31:56 +0200

    Clean up

--- FluxorExplorerUITests/FluxorExplorerUITests.swift
+++ FluxorExplorerUITests/FluxorExplorerUITests.swift
@@ -18,32 +18,15 @@ class FluxorExplorerUITests: XCTestCase {
         let app = XCUIApplication()
         app.launchEnvironment["UI_TESTING"] = "1"
         app.launch()
-        setupSnapshot(app)
         // Select peer
         let peerButton = app.tables.buttons["iPhone 11 Pro"]
         waitForElement(element: peerButton)
         peerButton.tap()
-        
-        
-        
-        
-        app.otherElements["SideAppDivider"].tap()
-        let element = app.otherElements["FluxorExplorer, iPhone 11 Pro"].scrollViews.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .other).element
-        element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .other).element.tap()
-        element.swipeUp()
-        
         // Select action
         let predicate = NSPredicate(format: "label CONTAINS[c] 'CompleteTodoAction'")
         let actionButton = app.tables.buttons.containing(predicate).element(boundBy: 0)
         waitForElement(element: actionButton)
         actionButton.tap()
-        // Take screenshot
-        snapshot("1-Selected-Action")
-        // Terminate
-        app.terminate()
-        
-        
-        
     }
 }
 
