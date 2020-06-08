64e3086e32c540ba3fd9fef85ab8c295af09d0e8
Morten Bjerg Gregersen <morten@mogee.dk>
2020-06-08 20:48:15 +0200

    Menu item for reopening the main window (#30)
    * Menu item for reopening the main window
    
    * Update AppDelegate.swift

---
+++ FluxorExplorer/AppDelegate+Menu.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate+Menu.swift
+//  FluxorExplorer
+//
+//  Created by Morten Bjerg Gregersen on 07/06/2020.
+//  Copyright © 2020 MoGee. All rights reserved.
+//
+
+import UIKit
+
+extension AppDelegate {
+    override func buildMenu(with builder: UIMenuBuilder) {
+        if builder.system == .main {
+            builder.insertChild(showDevicesListMenu(), atEndOfMenu: .window)
+        }
+    }
+
+    override func validate(_ command: UICommand) {}
+
+    private func showDevicesListMenu() -> UIMenu {
+        let showDevicesListCommand =
+            UIKeyCommand(title: "Show Devices List",
+                         image: nil,
+                         action: #selector(AppDelegate.showDevicesList),
+                         input: "D",
+                         modifierFlags: .command,
+                         propertyList: nil)
+        let showDevicesListMenu =
+            UIMenu(title: "",
+                   image: nil,
+                   identifier: UIMenu.Identifier("dev.fluxor.Explorer.menus.showDevicesList"),
+                   options: .displayInline,
+                   children: [showDevicesListCommand])
+        return showDevicesListMenu
+    }
+
+    @objc func showDevicesList() {
+        UIApplication.shared.requestSceneSessionActivation(nil,
+                                                           userActivity: SceneConfiguration.default.activity,
+                                                           options: nil, errorHandler: nil)
+    }
+}
