d7dbca5a8dab155f6668bcbfd86f49fc453c323f
Morten Bjerg Gregersen <morten@mogee.dk>
2020-06-08 20:40:03 +0200

    Update AppDelegate.swift

--- FluxorExplorer/AppDelegate.swift
+++ FluxorExplorer/AppDelegate.swift
@@ -9,7 +9,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil)
+        -> Bool {
         return true
     }
 
